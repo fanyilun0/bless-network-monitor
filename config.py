f19c15c24f811e23e289536625dcbad9f2a5ee5d
fanyilun <fanyilun0@gmail.com>
2024-11-22 17:44:53 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+# API配置
+API_URL = "https://gateway-run.bls.dev/api/v1/nodes"
+API_TOKEN = ""
+
+# Webhook配置
+WEBHOOK_URL = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key='
+
+# 代理配置
+PROXY_URL = 'http://localhost:7890'
+USE_PROXY = False
+ALWAYS_SEND = True
+# 时间配置
+INTERVAL = 300
+TIME_OFFSET = 8  
